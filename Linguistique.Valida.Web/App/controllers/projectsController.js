6c78591d5860cf921f70324b8c671868d3a6e343
AEonAX <sv.xeon@gmail.com>
2015-11-08 23:20:15 +0530

    Started with Angular, Material UI

---
+++ Linguistique.Valida.Web/App/controllers/projectsController.js
@@ -0,0 +1,244 @@
+﻿app.controller('ProjectsController', function ($scope) {
+    $scope.phones = [
+      { type: 'Home', number: '(555) 251-1234' },
+      { type: 'Cell', number: '(555) 786-9841' },
+      { type: 'Office', number: '(555) 314-1592' }
+    ];
+    $scope.todos = [
+      {
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+      },
+    ];
+
+
+    $scope.selected=[];
+    $scope.projects = [
+      {
+          _id: "563f6ee92c7cefa349767c8a",
+          ProjectName: "TVT R2018x",
+          ManagerName: "TATE Melva",
+          duration: "09/04/2014 to 08/12/2015",
+          participants: 2,
+          state: "Completed"
+      },
+      {
+          _id: "563f6ee9ec7940776f86655c",
+          ProjectName: "TVT R2010x",
+          ManagerName: "PHELPS Phoebe",
+          duration: "01/21/2014 to 08/02/2014",
+          participants: 5,
+          state: "In Progress"
+      },
+      {
+          _id: "563f6ee9a7a71fe5815df0d8",
+          ProjectName: "TVT R2015x",
+          ManagerName: "SALINAS Elsa",
+          duration: "03/15/2014 to 08/16/2014",
+          participants: 20,
+          state: "In Progress"
+      },
+      {
+          _id: "563f6ee98acbb19f655394e2",
+          ProjectName: "TVT R2002x",
+          ManagerName: "ROSA Valenzuela",
+          duration: "03/29/2015 to 11/16/2014",
+          participants: 8,
+          state: "In Progress"
+      },
+      {
+          _id: "563f6ee95744bc39d42e33d0",
+          ProjectName: "TVT R2009x",
+          ManagerName: "CALDERON Terra",
+          duration: "03/29/2015 to 12/25/2014",
+          participants: 9,
+          state: "Opened"
+      },
+      {
+          _id: "563f6ee971f953092248bcc3",
+          ProjectName: "TVT R2009x",
+          ManagerName: "NOLAN Alfreda",
+          duration: "03/19/2015 to 03/18/2014",
+          participants: 26,
+          state: "Opened"
+      },
+      {
+          _id: "563f6ee93bc1a02aa9fc5435",
+          ProjectName: "TVT R2010x",
+          ManagerName: "FISCHER Lynn",
+          duration: "09/22/2014 to 06/16/2015",
+          participants: 22,
+          state: "In Progress"
+      },
+      {
+          _id: "563f6ee9ed231addd44ecd07",
+          ProjectName: "TVT R2014x",
+          ManagerName: "LYNN Weeks",
+          duration: "01/11/2015 to 03/31/2015",
+          participants: 14,
+          state: "Completed"
+      },
+      {
+          _id: "563f6ee9ecbbee08eaea4340",
+          ProjectName: "TVT R2004x",
+          ManagerName: "KINNEY Gwendolyn",
+          duration: "08/04/2014 to 01/19/2014",
+          participants: 19,
+          state: "Completed"
+      },
+      {
+          _id: "563f6ee9fd08c7132633db0e",
+          ProjectName: "TVT R2003x",
+          ManagerName: "BUCHANAN Hoover",
+          duration: "07/04/2015 to 04/29/2014",
+          participants: 28,
+          state: "Completed"
+      },
+      {
+          _id: "563f6ee9a5f533833557e3da",
+          ProjectName: "TVT R1999x",
+          ManagerName: "ROJAS Randall",
+          duration: "06/06/2014 to 03/17/2014",
+          participants: 11,
+          state: "In Progress"
+      },
+      {
+          _id: "563f6ee9fa2d26c150279a6e",
+          ProjectName: "TVT R2018x",
+          ManagerName: "GAY Johanna",
+          duration: "07/10/2015 to 02/08/2015",
+          participants: 17,
+          state: "Completed"
+      },
+      {
+          _id: "563f6ee9da99443a6ac9f87d",
+          ProjectName: "TVT R2000x",
+          ManagerName: "WEAVER Riggs",
+          duration: "09/16/2015 to 05/06/2015",
+          participants: 18,
+          state: "In Progress"
+      },
+      {
+          _id: "563f6ee9f7eeeccef85bc9ba",
+          ProjectName: "TVT R2008x",
+          ManagerName: "GIBSON Dorothea",
+          duration: "04/10/2015 to 07/10/2015",
+          participants: 18,
+          state: "Completed"
+      },
+      {
+          _id: "563f6ee95986b120fb78fb87",
+          ProjectName: "TVT R1997x",
+          ManagerName: "LEE Misty",
+          duration: "09/26/2015 to 10/14/2014",
+          participants: 12,
+          state: "Completed"
+      },
+      {
+          _id: "563f6ee9b1fd033ba75ce03b",
+          ProjectName: "TVT R2019x",
+          ManagerName: "BAUER Boyd",
+          duration: "03/24/2014 to 04/30/2014",
+          participants: 28,
+          state: "Opened"
+      },
+      {
+          _id: "563f6ee9ae1fd956a4093d27",
+          ProjectName: "TVT R2014x",
+          ManagerName: "WALSH Russo",
+          duration: "08/14/2015 to 09/24/2015",
+          participants: 13,
+          state: "Opened"
+      },
+      {
+          _id: "563f6ee9905c1445246d8552",
+          ProjectName: "TVT R2009x",
+          ManagerName: "FARRELL Alberta",
+          duration: "08/01/2014 to 04/17/2015",
+          participants: 12,
+          state: "Opened"
+      },
+      {
+          _id: "563f6ee970c3b741d779f346",
+          ProjectName: "TVT R2019x",
+          ManagerName: "CARROLL Baxter",
+          duration: "08/27/2014 to 01/03/2014",
+          participants: 23,
+          state: "Opened"
+      },
+      {
+          _id: "563f6ee97adecf7499071ae4",
+          ProjectName: "TVT R1997x",
+          ManagerName: "ONEIL Adams",
+          duration: "06/12/2014 to 02/04/2015",
+          participants: 19,
+          state: "Completed"
+      },
+      {
+          _id: "563f6ee95df7a1118c390b33",
+          ProjectName: "TVT R2007x",
+          ManagerName: "WYATT Mcknight",
+          duration: "08/18/2014 to 02/22/2015",
+          participants: 2,
+          state: "Completed"
+      },
+      {
+          _id: "563f6ee9860c5651ec68d468",
+          ProjectName: "TVT R2019x",
+          ManagerName: "MOSS Yvette",
+          duration: "10/05/2015 to 12/09/2014",
+          participants: 3,
+          state: "Opened"
+      },
+      {
+          _id: "563f6ee973c11c8549ad8b3e",
+          ProjectName: "TVT R2014x",
+          ManagerName: "JOHNSON Roberta",
+          duration: "06/27/2014 to 10/30/2015",
+          participants: 4,
+          state: "In Progress"
+      },
+      {
+          _id: "563f6ee96babcf021d582cb6",
+          ProjectName: "TVT R2006x",
+          ManagerName: "SMITH Pierce",
+          duration: "04/24/2014 to 08/09/2014",
+          participants: 27,
+          state: "Opened"
+      },
+      {
+          _id: "563f6ee94941737226c99f30",
+          ProjectName: "TVT R1996x",
+          ManagerName: "SALAZAR Candace",
+          duration: "10/12/2014 to 08/28/2014",
+          participants: 26,
+          state: "In Progress"
+      }
+    ];
+});
--- Linguistique.Valida.Web/App_Start/RouteConfig.cs
+++ Linguistique.Valida.Web/App_Start/RouteConfig.cs
@@ -13,9 +13,14 @@ namespace Linguistique.Valida.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "index.html",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
